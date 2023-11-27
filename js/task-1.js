2f5bcdec936ecb78734b039b5d1e7b8e2a452ebc
Veakor <135655247+Veakor@users.noreply.github.com>
2023-11-27 16:07:42 +0100

    add task 1,2,3

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+function slugify(title) {
+    
+    const lowerCaseTitle = title.toLowerCase();
+    
+    const words = lowerCaseTitle.split(' ');
+    
+    const slug = words.join('-');
+
+    return slug;
+}
+
+console.log(slugify("Arrays for beginners")); 
+console.log(slugify("English for developers")); 
+console.log(slugify("Ten secrets of JavaScript")); 
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
